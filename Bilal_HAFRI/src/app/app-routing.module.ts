3f577b887d1122af478927c952fd0ba454e7a0b8
Bilal Hafri <hafribilal@gmail.com>
2021-04-14 18:04:51 +0000

    configure routing & app-module

--- Bilal_HAFRI/src/app/app-routing.module.ts
+++ Bilal_HAFRI/src/app/app-routing.module.ts
@@ -1,15 +1,34 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { canActivate, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
+
+// Send unauthorized users to login
+const redirectUnauthorizedToLogin = () =>
+  redirectUnauthorizedTo(['/login']);
+
+// Automatically log in users
+const redirectLoggedInToClient = () => redirectLoggedInTo(['/clients']);
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'login',
+    loadChildren: () => import('./Account/login/login.module').then(m => m.LoginPageModule),
+    ...canActivate(redirectLoggedInToClient),
+  },
+  {
+    path: 'clients',
+    ...canActivate(redirectUnauthorizedToLogin),
+    loadChildren: () => import('./Client/client.module').then(m => m.ClientModule)
+  },
+  {
+    path: 'tasks',
+    ...canActivate(redirectUnauthorizedToLogin),
+    loadChildren: () => import('./Task/task.module').then(m => m.TaskModule)
   },
 ];
 
