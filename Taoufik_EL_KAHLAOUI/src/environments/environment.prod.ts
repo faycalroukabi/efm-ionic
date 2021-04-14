e22af3b76c0fec407a63884ddb307eaf8385ec15
TaoufikElkahlaoui <tawfik.elkahlaoui@gmail.com>
2021-04-14 23:09:00 +0000

    Taoufik

---
+++ Taoufik_EL_KAHLAOUI/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCIAEOEsVntgwIDl5oV2-5aRjui-KH_nv4",
+    authDomain: "ionic-first-app-ad556.firebaseapp.com",
+    databaseURL: "https://ionic-first-app-ad556-default-rtdb.firebaseio.com",
+    projectId: "ionic-first-app-ad556",
+    storageBucket: "ionic-first-app-ad556.appspot.com",
+    messagingSenderId: "44064151706",
+    appId: "1:44064151706:web:c371153c7ab427f916e723",
+    measurementId: "G-7C1SXCH2R2"
+  }
+};
