dd1c92559192f2289ab6e3d45cdb8efdaf4788d7
Bawliet23 <mohamedbhajy23@gmail.com>
2021-04-15 01:32:10 +0200

    no message

--- mohamed_bhajy/restaurant/src/main/java/com/example/restaurant/reporsitories/IProductRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.restaurant.reporsitories;
-
-import com.example.restaurant.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IProductRepo extends JpaRepository<Product,Long> {
-
-}
--- mohamed_bhajy/restaurant/src/main/java/com/example/restaurant/reporsitories/IProductsOrder.java
+++
@@ -1,9 +0,0 @@
-package com.example.restaurant.reporsitories;
-
-
-import com.example.restaurant.entities.ProductsOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IProductsOrder extends JpaRepository<ProductsOrder,Long> {
-
-}
